911f2718a19949c324d719ebe9c0469aead70036
gabriellagal <32105752+gabriellagal@users.noreply.github.com>
2017-11-25 19:30:36 +0000

    Update bot_egg.py

--- bot_egg.py
+++ bot_egg.py
@@ -11,10 +11,10 @@ test_number_1 = tweetbot.generate_text(20, seedword =['a'])
 print("tweetbot says:")
 print(test_number_1)
 
-cons_key = 'FxTN3P2LTXwv2yV0x7i2biEQb'
-cons_secret = 'qy9ovcHsOnnIrfsNUGvo3hrWUt0NybzZ6zMgdaRsRVEpwQ6sry'
-access_token = '934377774713856000-X6F11aI90IbKhBUqMe40sHDmPu3C5ec'
-access_token_secret = 'H4mhxOVm9AGruXCkNga5O7I5UhgB7dmbBTe2tyxtSQFva'
+cons_key = ''
+cons_secret = ''
+access_token = '-'
+access_token_secret = ''
 
 tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
 
