e793453a100fa5ea2788be652c46d824c95291b9
gabriellagal <32105752+gabriellagal@users.noreply.github.com>
2017-11-25 15:30:25 +0000

    Add files via upload

---
+++ randomize.py
@@ -0,0 +1,25 @@
+import random
+from random import shuffle
+
+def shuffleadj():
+    with open("adjective.txt", mode="r") as adjmix:
+    	words = []
+    	for word in adjmix:
+    		words.append(word.rstrip('\n'))
+    	return random.choice(words) 
+
+def shufflenoun():
+    with open("noun.txt", mode="r") as nounmix:
+    	words = []
+    	for word in nounmix:
+    		words.append(word.rstrip('\n'))
+    	return random.choice(words) 
+
+def shuffleteam():
+    with open("team.txt".rstrip(), mode="r") as teammix:
+    	words = []
+    	for word in teammix:
+    		words.append(word)
+    	return random.choice(words) 
+
+print "New team name: " + shuffleadj() + ' ' + shufflenoun() + ' ' + shuffleteam()
