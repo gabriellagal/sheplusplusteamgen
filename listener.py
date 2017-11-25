14386bb1d5b69c1850702bb1c665bb57b2c841cd
AlexMillward <millwarda@googlemail.com>
2017-11-25 13:28:57 +0000

    Added default response for listener

--- listener.py
+++ listener.py
@@ -7,23 +7,28 @@ config = None
 
 class FollowListener(tweepy.StreamListener):
 	
+	def __init__(self, api):
+		self.api = api
+
 	def on_status(self, status):
-		print status.text
+		print 'Responding to ' + status.user.screen_name
+		text = 'I don\'t know what you want @' + status.user.screen_name + ' :('
+		self.api.update_status(text)
 
 def main():
 	global config
 	
-	with open("/root/python/sheplusplusteamgen/config.json", "r") as f:
+	with open('/root/python/sheplusplusteamgen/config.json', 'r') as f:
 		data = f.read()
 		config = json.loads(data)
 	
-	auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
-	auth.set_access_token(config["access_token"], config["access_token_secret"])
+	auth = tweepy.OAuthHandler(config['consumer_key'], config['consumer_secret'])
+	auth.set_access_token(config['access_token'], config['access_token_secret'])
 	api = tweepy.API(auth)
-	listener = FollowListener()
+	listener = FollowListener(api)
 	stream = tweepy.Stream(auth = api.auth, listener=listener)
 
-	print "Listening"
-	stream.filter(track=["@ShePlusPlusTeam"])
+	print 'Listening'
+	stream.filter(track=['@ShePlusPlusTeam'])
 
 main()
