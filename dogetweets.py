beb2fd199ab659a9015c9a4cd6801351394e5533
gabriellagal <32105752+gabriellagal@users.noreply.github.com>
2017-11-26 10:13:09 +0000

    fixed capitalisation error

--- dogetweets.py
+++ dogetweets.py
@@ -1,29 +1,32 @@
 import tweepy
-import inflection
 import random
 from random import shuffle
+
+def normalise(word):
+  word = word.rstrip('\n')
+  word = word[0].upper() + word[1:]
+  return word
 
 def shuffleadj():
     with open("adjective.txt", mode="r") as adjmix:
       words = []
       for word in adjmix:
-        words.append(word.rstrip('\n'))
-        return inflection.titleize(random.choice(words)) 
+        words.append(normalise(word))
+      return random.choice(words) 
 
 def shufflenoun():
     with open("noun.txt", mode="r") as nounmix:
       words = []
       for word in nounmix:
-        words.append(word.rstrip('\n'))
-        return random.choice(words)
-        return inflection.titleize(words) 
+        words.append(normalise(word))
+      return random.choice(words) 
 
 def shuffleteam():
     with open("team.txt".rstrip(), mode="r") as teammix:
       words = []
       for word in teammix:
-        words.append(word.rstrip('\n'))
-        return inflection.titleize(random.choice(words)) 
+        words.append(normalise(word))
+      return random.choice(words) 
 
 def get_api(cfg):
   auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
@@ -33,14 +36,14 @@ def get_api(cfg):
 def main():
   # Fill in the values noted in previous step here
   cfg = { 
-    "consumer_key"        : "",
-    "consumer_secret"     : "",
-    "access_token"        : "",
-    "access_token_secret" : "" 
+    "consumer_key"        : "FxTN3P2LTXwv2yV0x7i2biEQb",
+    "consumer_secret"     : "qy9ovcHsOnnIrfsNUGvo3hrWUt0NybzZ6zMgdaRsRVEpwQ6sry",
+    "access_token"        : "934377774713856000-X6F11aI90IbKhBUqMe40sHDmPu3C5ec",
+    "access_token_secret" : "H4mhxOVm9AGruXCkNga5O7I5UhgB7dmbBTe2tyxtSQFva" 
     }
 
   api = get_api(cfg)
-  tweet = "New team name: " + shuffleadj() + ' ' + shufflenoun() + ' ' + shuffleteam()
+  tweet = "Here's a team name suggestion" + shuffleadj() + ' ' + shufflenoun() + ' ' + shuffleteam() 
   status = api.update_status(status = tweet) 
   # Yes, tweet is called 'status' rather confusing
 
